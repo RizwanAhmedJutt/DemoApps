bff3ae83f5248edd155be6f3230102bbc6707ef1
RizwanAhmedJutt <rizwan6768@gmail.com>
2016-03-04 01:10:17 +0500

    Add Button

--- GethubApps/Form1.cs
+++ GethubApps/Form1.cs
@@ -16,5 +16,10 @@ namespace GethubApps
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button Clicked !!!");
+        }
     }
 }
